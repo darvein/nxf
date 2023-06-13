16533b5202fd1a261db960b9525296f01631e9f4
Dennis Ruiz <darvein@gmail.com>
2023-06-12 21:55:14 -0400

    updating project

--- nxd.sh
+++ nxd.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+# Define usage function
+usage(){
+  echo "Usage: $0 filepath pattern"
+  echo "filepath: pattern to match in the file names"
+  echo "pattern: pattern to search in the file content"
+  exit 1
+}
 
-if [ $# -eq 2 ]; then
-  FILEPATH_PATTERN="${1}"
-  PATTERN="${2}"
-elif [ $# -eq 1 ]; then
-  FILEPATH_PATTERN="${1}"
-  PATTERN=""
-else
-    echo "No parameters were passed."
-    exit 0
+# Check number of arguments
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+  usage
 fi
 
-find "${SCRIPT_PATH}/snippets" -type f -path "*$FILEPATH_PATTERN*" -exec awk '
-    /^# [A-Za-z]:/ {
-        if (block != "")
-            print block
-        block = $0
-        next
+# Define file path pattern and pattern to search
+FILEPATH_PATTERN="${1}"
+PATTERN="${2:-}"  # Default to empty string if not provided
+
+SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+
+# Find files that match the file path pattern and process them
+find "${SCRIPT_PATH}/snippets" -type f -path "*${FILEPATH_PATTERN}*" |
+while read -r file; do
+  awk -v pattern="${PATTERN}" -v RS='-----' '
+    BEGIN {IGNORECASE = 1}
+    $0 ~ pattern {
+        if ($0 != "") {
+            print $0
+        }
     }
-    {
-        block = block ORS $0
-    }
-    END {
-        if (block != "")
-            print block
-    }
-' {} + \
-  | awk -v RS= -v ORS='\n\n' -v pattern="$PATTERN" 'tolower($0) ~ tolower(pattern)' \
-  | xargs -0 -I '{}' echo -e '{}'
+  ' "${file}" | sed '/^$/N;/\n$/D'
+done
--- snippets/python/fibonacci.py
+++ snippets/python/fibonacci.py
@@ -3,16 +3,6 @@ def fibonacci(n):
     a = 0
     b = 1
 
-# Driver Program
-
-print(fibonacci(9))
 print(fibonacci(9))
 
-# Example 2: Nones function
-def nones(n):
-    a = 0
-    b = 1
-
-# Driver Program
-
-print(nones(9))
+-----
