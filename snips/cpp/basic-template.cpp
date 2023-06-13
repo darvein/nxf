cdbcae8acd3cd1e9d24bdcfa872eded970d480e1
Dennis Ruiz <darvein@gmail.com>
2023-06-13 12:36:36 -0400

    updating

---
+++ snips/cpp/basic-template.cpp
@@ -0,0 +1,14 @@
+# Basic template
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    cout << "hey" << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+}
+-----
